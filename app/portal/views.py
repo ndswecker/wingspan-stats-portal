14e5dda8cb1bad7240db6a44a6d5526457e789d4
Nathanael Swecker <nathanael.swecker@gmail.com>
2026-07-04 10:17:48 -0700

    render the player scores - basic

--- app/portal/views.py
+++ app/portal/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import Game, GameResult
 
 # Create your views here.
 def game_history(request):
-    return render(request, "portal/game_history.html")
+    games = (
+        Game.objects
+        .prefetch_related("results__player")
+        .order_by("-date_played", "-id")[:25]
+    )
+
+    game_rows = []
+    for game in games:
+        results = list(game.results.all())
+        game_rows.append({
+            "id": game.id,
+            "date_played": game.date_played,
+            "player_one": results[0],
+            "player_two": results[1],
+        })
+
+    return render(
+        request, 
+        "portal/game_history.html", 
+        {"game_rows": game_rows}
+    )
